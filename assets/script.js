33d6c1cdbcf04b1030caee906163bf0cc7fb5e2a
David Rojo <rojo@rojorevolution.com>
2021-01-05 19:01:40 -0600

    Commiting with an object in JS file, could not figure it out how to iterate so will try arrays instead

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,58 @@
 //quiz from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
+//Object
+// var quiz = {
+//     questions: {
+//         q1: "Inside which HTML element do we put the JavaScript?",
+//         q2: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+//         q3: "The external JavaScript file must contain the <script> tag.",
+//     },
+//     q1Answer: {
+//         option1: "<script>",
+//         option2: "<javascript>",
+//         option3: "<scripting>",
+//         option4: "<js>",
+//     },
+//     q2Answer: {
+//         option1: "<script src='xxx.js'",
+//         option2: "<script name='xxx.js'",
+//         option3: "<script href='xxx.js'",
+//     },
+//     q3Answer: {
+//         option1: 'True',
+//         option2: 'False'
+//     }
+// }
 
-var quiz = {
-    question1: "Inside which HTML element do we put the JavaScript?",
-    q1Answer: {
-        option1: "<script>",
-        option2: "<javascript>",
-        option3: "<scripting>",
-        option4: "<js>"
-    },
-    question2: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    q2Answer: {
-        option1: "<script src='xxx.js'",
-        option2: "<script name='xxx.js'",
-        option3: "<script href='xxx.js'"
-    },
-    question3: "The external JavaScript file must contain the <script> tag.",
-    q3Answer: {
-        option1: 'True',
-        option2: 'False'
-    }
+
+
+
+//Variables
+var timeEl = document.querySelector(".timer");
+var questionEl = document.querySelector(".question");
+var answerEl = document.querySelector(".answers");
+
+var creatBtn = document.createElement("button");
+
+var maxTime = 75;
+
+//Function to Start Timer
+function startTime() {
+    var timerInterval = setInterval(function () {
+        maxTime--;
+        timeEl.textContent = maxTime;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
 }
 
-var questionEl = document.createElement("h2");
-var answerEl = document.createElement("button");
+//Function to insert Questions
+function quesInsert() {
+    questionEl.textContent = quiz.questions.q1;
+    answerEl.appendChild(creatBtn);
+    creatBtn.setAttribute("class", "button")
+    creatBtn.textContent = quiz.q1Answer.option1;
+}
+quesInsert()
+// startTime();
