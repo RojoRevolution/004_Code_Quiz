5e964fb57d6e528d604658441ec08e6bbbbef8ff
David Rojo <rojo@rojorevolution.com>
2021-01-07 15:06:24 -0600

    restarting script because I feel lost

--- assets/script.js
+++ assets/script.js
@@ -18,65 +18,8 @@ var startArea = document.querySelector(".startArea");
 var goBtn = document.querySelector(".goBtn");
 var nextBtn = document.querySelector(".next");
 var questionDiv = document.querySelector(".question-block");
+var questionEl = document.querySelector(".question")
+var answerBlock = document.querySelector(".answer-buttons");
 var answerDiv = document.querySelector(".answers");
 var statusShow = document.querySelector(".show");
 var statusHide = document.querySelector(".hide");
-
-var allChoiceBtns = document.querySelectorAll(".choices")
-
-var currentQuestion = 0;
-var score = 0;
-var timeLimit = 100;
-// const answerOptions = document.querySelectorAll('.choices');
-
-
-function setTime() {
-    var timerInterval = setInterval(function () {
-        timeLimit--;
-        timeEl.textContent = `Time Left: ${timeLimit}`;
-
-        if (timeLimit === 0) {
-            clearInterval(timerInterval);
-            console.log("Times Up")
-        }
-
-    }, 1000);
-};
-
-function start() {
-    startArea.classList.add("hide");
-    questionDiv.classList.remove("hide");
-    setTime();
-    renderQuiz();
-}
-
-function renderQuiz() {
-    var askQuestions = allQuestions[currentQuestion].question;
-    document.querySelector(".question").innerHTML = askQuestions;
-    //Answers
-    var showChoices = allQuestions[currentQuestion].choices;
-    for (var i = 0; i < allQuestions[currentQuestion].choices.length; i++) {
-        choiceBtn = document.createElement("button");
-        choiceBtn.textContent = showChoices[i];
-        choiceBtn.setAttribute("class", "btn btn-primary m-2 choices");
-        choiceBtn.setAttribute("value", [i]);
-        answerDiv.appendChild(choiceBtn);
-    }
-    currentQuestion++;
-}
-
-
-// function showNextQuestion() {
-//     askQuestion()
-//     showChoices()
-// }
-
-
-goBtn.addEventListener("click", function () {
-    console.log("click");
-    start();
-})
-
-
-
-
